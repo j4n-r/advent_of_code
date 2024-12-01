630aae27460dd47e8535bf3f08e3b503d9c77e50
j4n-r <jan.ruegge@gmail.com>
2024-12-01 17:57:53 +0100

    day one

---
+++ 2024/day_1_historian_hysteria/go.mod
@@ -0,0 +1,3 @@
+module github.com/j4n-r/advent_of_code/tree/main/2024/day_1_historian_hysteria
+
+go 1.23.3
--- 2024/day_1_historian_hysteria/main.go
+++ 2024/day_1_historian_hysteria/main.go
@@ -1,15 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	leftIds := [1000]int
-	rightIds := [1000]int
-	distances := [1000]int
+	leftIds := []int{}
+	rightIds := []int{}
 
-	os.open("input.txt")
+	readFile, err := os.Open("input.txt")
+	defer readFile.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
 
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		idStrings := strings.Split(line, "  ")
+
+		idLeft, err := strconv.Atoi(strings.TrimSpace(idStrings[0]))
+		idRight, err := strconv.Atoi(strings.TrimSpace(idStrings[1]))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		leftIds = append(leftIds, idLeft)
+		rightIds = append(rightIds, idRight)
+	}
+	slices.Sort(leftIds)
+	slices.Sort(rightIds)
+
+	sum := 0
+	for i := range leftIds {
+		distance := leftIds[i] - rightIds[i]
+		distance = int(math.Abs(float64(distance)))
+		sum = sum + distance
+		// fmt.Printf("%d \t %d \t distance: %d\n", leftIds[i], rightIds[i], distance)
+	}
+
+	fmt.Printf("sum: %d ", sum)
+	partTwo(leftIds, rightIds)
+
+}
+
+func partTwo(leftIds []int, rightIds []int) {
+
+	smScore := 0
+	for _, vl := range leftIds {
+		counter := 0
+		for _, vr := range rightIds {
+			if vl == vr {
+				counter++
+			}
+		}
+		smScore += vl * counter
+	}
+	fmt.Println("smScore: ", smScore)
 }
