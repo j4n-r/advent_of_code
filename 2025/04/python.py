b1d0071caf9ca8a085e9117624263b56e62461fc
j4n-r <jan.rueggeberg@pm.me>
2025-12-04 20:22:45 +0100

    scuffed part 1 day 4

--- 2025/04/python.py
+++ 2025/04/python.py
@@ -1,16 +1,67 @@
 def parse_input() -> list[str]:
-    f = open("test_input.txt")
+    f = open("input.txt")
     content = f.read()
-    return content.split()
+    lines = content.split()
+    out = []
+    for line in lines:
+        out.append( list(line))
+    return out
 
+def print_grid(grid):
+    for line in grid:
+        print("".join(line))
+    
 
 def check_rolls():
     content = parse_input()
     window_len = 8
 
-    adjacent = []
-    for line in content:
-        for char in line:
+    # ..@@.@@@@.
+    # @@@.@.@.@@
+    # @@@@@.@.@@
+    # @.@@@@..@.
+
+    sum = 0
+
+    grid = []
+    for i in range(len(content)):
+        for j in range(len(content[0])):
+            adjacent: list[str]= []
+            # check row on top
+            if content[i][j] == '@':
+                if i != 0:
+                    if j != 0:
+                        adjacent.append(content[i-1][j-1])
+                    adjacent.append(content[i-1][j])
+                    if j != len(content[0]) - 1:
+                        adjacent.append(content[i-1][j+1])
+
+                # check same row
+
+                if j != 0:
+                    adjacent.append(content[i][j-1])
+
+                if j != len(content[0]) - 1:
+                    adjacent.append(content[i][j+1])
+
+                # check last row
+                if i != len(content)-1:
+                    if j != 0:
+                        adjacent.append(content[i+1][j-1])
+                    adjacent.append(content[i+1][j])
+                    if j != len(content[0]) - 1:
+                        adjacent.append(content[i+1][j+1])
+
+                if adjacent.count('@') + adjacent.count('X') < 4:
+                    content[i][j] = 'X'
+                    print(adjacent)
+                    sum += 1
+
+    print_grid(content)
+    print(sum)
 
 
 check_rolls()
+
+#print(parse_input())
+
